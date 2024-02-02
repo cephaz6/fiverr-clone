6f8347b369452db73d46756048618f24728c3ade
Cephaz6 <cephasblog@gmail.com>
2024-02-02 21:03:02 +0000

    Footer Component Fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,79 @@
 import React from "react";
 import "./Footer.scss";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+          </div>
+          <div className="item">
+            <h2>About</h2>
+            <span>Privacy Policy</span>
+            <span>Terms of Service</span>
+            <span>Intellectual Property Claims</span>
+            <span>Investor Relations</span>
+            <span>Contact Sales</span>
+          </div>
+          <div className="item">
+            <h2>Support</h2>
+            <span>Help & Support</span>
+            <span>Trust & Safety</span>
+            <span>Selling on Cephaz</span>
+            <span>Buying on Cephaz</span>
+          </div>
+          <div className="item">
+            <h2>Community</h2>
+            <span>Affiliates</span>
+            <span>Podcast</span>
+            <span>Invite a Friend</span>
+            <span>Become a Seller</span>
+            <span>Community Standards</span>
+          </div>
+          <div className="item">
+            <h2>More From Fiverr</h2>
+            <span>Cephaz Business</span>
+            <span>Cephaz Pro</span>
+            <span>Cephaz Logo Maker</span>
+            <span>Cephaz Guides</span>
+            <span>Get Inspired</span>
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h2>Cephaz.</h2>
+            <span>&copy; Cephaz - Tindax Technologies, 2024</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="/img/twitter.png" alt="" />
+              <img src="/img/facebook.png" alt="" />
+              <img src="/img/linkedin.png" alt="" />
+              <img src="/img/pinterest.png" alt="" />
+              <img src="/img/instagram.png" alt="" />
+            </div>
+            <div className="link">
+              <img src="/img/language.png" alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src="/img/coin.png" alt="" />
+              <span>USD</span>
+            </div>
+            <img src="/img/accessibility.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
