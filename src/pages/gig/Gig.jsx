f081a56a0e301fec8de3cabacca7a9c72545ba0d
Cephaz6 <cephasblog@gmail.com>
2024-02-09 20:00:56 +0000

    Gig Page Completed

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -1,8 +1,202 @@
 import React from "react";
 import "./Gig.scss";
+import { Slider } from "infinite-react-carousel/lib";
 
 const Gig = () => {
-  return <div>Gig</div>;
+  return (
+    <div className="gig">
+      <div className="container">
+        <div className="left">
+          <span className="breadCrumbs">Cephaz > Wordpress Design</span>
+          <h1>I Will Create AI Generated Art For You</h1>
+
+          <div className="user">
+            <img
+              className="gigProfilePicture"
+              src="https://media.licdn.com/dms/image/D4D03AQG0huJRM1SAgw/profile-displayphoto-shrink_800_800/0/1687422439969?e=2147483647&v=beta&t=aiEyVdskUcR8eBv0CWWYqAb1_o2s67xUL70mG4dnnso"
+              alt=""
+            />
+            <span>Cephas Ose</span>
+            <div className="stars">
+              <img src="../img/star.png" alt="" />
+              <img src="../img/star.png" alt="" />
+              <img src="../img/star.png" alt="" />
+              <img src="../img/star.png" alt="" />
+              <img src="../img/star.png" alt="" />
+              <span>5</span>
+            </div>
+          </div>
+          <Slider slidesToShow={1} arrowsScroll={1} className="slider">
+            <img
+              src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <img
+              src="https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <img
+              src="https://images.pexels.com/photos/1054777/pexels-photo-1054777.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+          </Slider>
+          <h2>About This Gig</h2>
+          <p>
+            I use an AI program to create images based on text prompts. This
+            means I can help you to create a vision you have through a textual
+            description of your scene without requiring any reference images.
+            Some things I've found it often excels at are: Character portraits
+            (E.g. a picture to go with your DnD character) Landscapes (E.g.
+            wallpapers, illustrations to compliment a story) Logos (E.g. Esports
+            team, business, profile picture) You can be as vague or as
+            descriptive as you want. Being more vague will allow the AI to be
+            more creative which can sometimes result in some amazing images. You
+            can also be incredibly precise if you have a clear image of what you
+            want in mind. All of the images I create are original and will be
+            found nowhere else. If you have any questions you're more than
+            welcome to send me a message.
+          </p>
+          <div className="seller">
+            <h2>About The Seller</h2>
+            <div className="user">
+              <img
+                src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="info">
+                <span>Cephas Oselumese</span>
+                <div className="stars">
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <span>5</span>
+                </div>
+                <button>Contact Me</button>
+              </div>
+            </div>
+            <div className="box">
+              <div className="items">
+                <div className="item">
+                  <span className="title">From</span>
+                  <span className="desc">USA</span>
+                </div>
+                <div className="item">
+                  <span className="title">Member since</span>
+                  <span className="desc">Aug 2022</span>
+                </div>
+                <div className="item">
+                  <span className="title">Avg. response time</span>
+                  <span className="desc">4 hours</span>
+                </div>
+                <div className="item">
+                  <span className="title">Last delivery</span>
+                  <span className="desc">1 day</span>
+                </div>
+                <div className="item">
+                  <span className="title">Languages</span>
+                  <span className="desc">English</span>
+                </div>
+              </div>
+              <hr />
+              <p>
+                My name is Oselumese Abainmun, I enjoy creating AI generated art
+                in my spare time. I have a lot of experience using the AI
+                program and that means I know what to prompt the AI with to get
+                a great and incredibly detailed result.
+              </p>
+            </div>
+          </div>
+          <div className="reviews">
+            <h2>Reviews</h2>
+            <div className="item">
+              <div className="user">
+                <img
+                  className="profilePicture"
+                  src="https://media.licdn.com/dms/image/D4D03AQG0huJRM1SAgw/profile-displayphoto-shrink_800_800/0/1687422439969?e=2147483647&v=beta&t=aiEyVdskUcR8eBv0CWWYqAb1_o2s67xUL70mG4dnnso"
+                  alt=""
+                />
+                <div className="info">
+                  <span>Cephas Ose</span>
+                  <div className="country">
+                    <img
+                      src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/62793/united-kingdom-flag-emoji-clipart-xl.png"
+                      alt=""
+                    />
+                    <span>United Kingdom</span>
+                  </div>
+                </div>
+              </div>
+              <div className="stars">
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <span>5</span>
+              </div>
+              <p>
+                I just want to say that art_with_ai was the first, and after
+                this, the only artist Ill be using on Fiverr. Communication was
+                amazing, each and every day he sent me images that I was free to
+                request changes to. They listened, understood, and delivered
+                above and beyond my expectations. I absolutely recommend this
+                gig, and know already that Ill be using it again very very soon
+              </p>
+              <div className="helpful">
+                <span>Helpful?</span>
+                <img src="/img/like.png" alt="" />
+                <span>Yes</span>
+                <img src="/img/dislike.png" alt="" />
+                <span>No</span>
+              </div>
+            </div>
+            <hr />
+          </div>
+        </div>
+        <div className="right">
+          <div className="price">
+            <h3>1 AI Generated Image</h3>
+            <h2>$ 69.99</h2>
+          </div>
+          <p>
+            I will create a unique high quality AI generated image based on a
+            description that you give me
+          </p>
+          <div className="details">
+            <div className="item">
+              <img src="/img/clock.png" alt="" />
+              <span>2 Days Delivery</span>
+            </div>
+            <div className="item">
+              <img src="/img/recycle.png" alt="" />
+              <span>3 Revisions</span>
+            </div>
+          </div>
+          <div className="features">
+            <div className="item">
+              <img src="/img/greencheck.png" alt="" />
+              <span>Prompt Writing</span>
+            </div>
+            <div className="item">
+              <img src="/img/greencheck.png" alt="" />
+              <span>Artwork Delivery</span>
+            </div>
+            <div className="item">
+              <img src="/img/greencheck.png" alt="" />
+              <span>Image Upscaling</span>
+            </div>
+            <div className="item">
+              <img src="/img/greencheck.png" alt="" />
+              <span>Additional Design</span>
+            </div>
+          </div>
+          <button>Continue</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Gig;
