66ef983d12263b8713dbe8dd9d6c12350900d9b0
Cephaz6 <cephasblog@gmail.com>
2024-02-10 16:29:32 +0000

    Add A gig Page Done

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -2,7 +2,66 @@ import React from "react";
 import "./Add.scss";
 
 const Add = () => {
-  return <div>Add</div>;
+  return (
+    <div className="add">
+      <div className="container">
+        <h1>Add New Gig</h1>
+        <div className="sections">
+          <div className="left">
+            <label htmlFor="">Title</label>
+            <input
+              type="text"
+              placeholder="e.g I want to do something I'm really good at"
+            />
+            <label htmlFor="">Category</label>
+            <select name="categories" id="">
+              <option value="Design">Design</option>
+              <option value="Web Devlopment">Web Devlopment</option>
+              <option value="AI Model">AI Model</option>
+              <option value="Animation">Animation</option>
+              <option value="Music">Music</option>
+            </select>
+            <label htmlFor="">Cover Image</label>
+            <input type="file" />
+            <label htmlFor="">Upload Images</label>
+            <input type="file" multiple />
+            <label htmlFor="">Description</label>
+            <textarea
+              placeholder="Brief description to introduce your service to customers"
+              name=""
+              id=""
+              cols="30"
+              rows="16"
+            ></textarea>
+            <button>Create</button>
+          </div>
+          <div className="right">
+            <label htmlFor="">Service Title</label>
+            <input type="text" placeholder="e.g. Single-page Web Design" />
+            <label htmlFor="">Short Description</label>
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Short description of your service"
+            ></textarea>
+            <label htmlFor="">Delivery Time (e.g 3 days)</label>
+            <input type="number" min={1} />
+            <label htmlFor="">Revision Number</label>
+            <input type="number" min={1} />
+            <label htmlFor="">Add Features</label>
+            <input type="text" placeholder="e.g. page design" />
+            <input type="text" placeholder="e.g. file uploading" />
+            <input type="text" placeholder="e.g. setting up a domain" />
+            <input type="text" placeholder="e.g. hosting" />
+            <label htmlFor="">Price</label>
+            <input type="number" min={1} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Add;
