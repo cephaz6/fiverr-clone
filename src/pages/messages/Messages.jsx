868a97a0c00fbe405b449090ca9e9e76e7f0cf2f
Cephaz6 <cephasblog@gmail.com>
2024-02-10 15:47:56 +0000

    Messages, Message, MyGigs and Orders pages Completed

--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -1,7 +1,106 @@
 import React from "react";
 import "./Messages.scss";
+import { Link } from "react-router-dom";
+
 const Messages = () => {
-  return <div>Messages</div>;
+  //  Active User Starts Here
+  const currentUser = {
+    id: 1,
+    username: "Oselumese",
+    isSeller: true,
+  };
+  // Active User Ends Here
+
+  const message =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores provident delectus harum autem fuga. Totam id at repellendus pariatur ipsa";
+
+  return (
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Buyer</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          <tr className="active">
+            <td>John Luke</td>
+            <td>
+              <Link className="link" to="/message/123">
+                {" "}
+                {message.substring(0, 100)}...{" "}
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr className="active">
+            <td>John Luke</td>
+            <td>
+              <Link className="link" to="/message/123">
+                {" "}
+                {message.substring(0, 100)}...{" "}
+              </Link>
+            </td>
+            <td>2 hours ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>John Luke</td>
+            <td>
+              <Link className="link" to="/message/123">
+                {" "}
+                {message.substring(0, 100)}...{" "}
+              </Link>
+            </td>
+            <td>1 day ago</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>John Luke</td>
+            <td>
+              <Link className="link" to="/message/123">
+                {" "}
+                {message.substring(0, 100)}...{" "}
+              </Link>
+            </td>
+            <td>3 days ago</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>John Luke</td>
+            <td>
+              <Link className="link" to="/message/123">
+                {" "}
+                {message.substring(0, 100)}...{" "}
+              </Link>
+            </td>
+            <td>3 days ago</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>John Luke</td>
+            <td>
+              <Link className="link" to="/message/123">
+                {" "}
+                {message.substring(0, 100)}...{" "}
+              </Link>
+            </td>
+            <td>3 days ago</td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Messages;
--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,7 +1,135 @@
 import React from "react";
 import "./Orders.scss";
+// import { Link } from "react-router-dom";
 const Orders = () => {
-  return <div>Orders</div>;
+  //  Active User Starts Here
+  const currentUser = {
+    id: 1,
+    username: "Oselumese",
+    isSeller: true,
+  };
+  // Active User Ends Here
+
+  return (
+    <div className="myOrders">
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{currentUser?.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Stunning concept art</td>
+            <td>
+              59.<sup>99</sup>
+            </td>
+            <td>Johnny Joe</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Ai generated concept art</td>
+            <td>
+              120.<sup>99</sup>
+            </td>
+            <td>Luka Modric</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>High quality digital character</td>
+            <td>
+              79.<sup>99</sup>
+            </td>
+            <td>Jude Bellingham</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Illustration hyper realistic painting</td>
+            <td>
+              119.<sup>99</sup>
+            </td>
+            <td>Toni Kross</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Original ai generated digital art</td>
+            <td>
+              59.<sup>99</sup>
+            </td>
+            <td>Adin Ross</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Text based ai generated art</td>
+            <td>
+              110.<sup>99</sup>
+            </td>
+            <td>Helen Benneth</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
